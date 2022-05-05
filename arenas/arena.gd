e64aa4ee283df393c8e57564e1cffe253f73f150
Naga <nb3052@nyu.edu>
2022-05-04 20:53:55 -0400

    Enemy places adjustment

--- arenas/arena.gd
+++ arenas/arena.gd
@@ -4,11 +4,11 @@ extends Spatial
 
 # First four values of the array copied from previous placements
 var pantera_positions = [Vector3(-99.75, 0, -8.85), Vector3(-98.69, 0, 93.58), 
-Vector3(1.20, 0, 130.53), Vector3(31.77, 0, -42.45), Vector3(-93.75, 0, -8.85), Vector3(-90.75, 0, -8.85), 
-Vector3(-108.75, 0, -8.85), Vector3(-99.75, 0, -8.85), Vector3(-99.75, 0, -8.85), Vector3(-99.75, 0, -8.85)]
+Vector3(1.20, 0, 130.53), Vector3(31.77, 0, -42.45), Vector3(3, 0, -6), Vector3(-14, 0, 11), 
+Vector3(-41, 0, -15), Vector3(5, 0, -55), Vector3(-46, 0, 13), Vector3(28, 0, 27)]
 var monster_positions = [Vector3(-99.40, 0, 49.85), Vector3(-52.50, 0, 151.60), Vector3(59.61, 0, -64.01), 
-Vector3(26.98, 0, 79.82), Vector3(-99.40, 0, 49.85), Vector3(-99.40, 0, 49.85), Vector3(-99.40, 0, 49.85),
-Vector3(-99.40, 0, 49.85), Vector3(-99.40, 0, 49.85), Vector3(-99.40, 0, 49.85)]
+Vector3(26.98, 0, 79.82), Vector3(20, 0, 72), Vector3(-62, 0, 93), Vector3(-90, 0, 65),
+Vector3(-52, 0, 124), Vector3(-20, 0, 173), Vector3(8, 0, 220)]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
