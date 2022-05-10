1d37332783735ffdf55261e5dacc28a6f2f086d0
Elvis Cabreja <ec3539@nyu.edu>
2022-05-10 17:48:35 -0400

    Added timer + adjusted endgame score

--- arenas/display.gd
+++ arenas/display.gd
@@ -1,12 +1,7 @@
 extends Label
 
+var score_calculated = false
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	# self.text = ""
 	# display_text()
@@ -21,22 +16,30 @@ func display_text():
 	self.text += '\n' + "Enemies left: " + str(Globals.enemies_left)
 	#self.text += '\n' + "Health Enemy2: " + str(Globals.enemy_health2)
 	#self.text += '\n' + "Speed(KPH):" + str(Globals.kph)
+	self.text += '\n' + "Time Elapsed: " + str(Globals.min_elapsed).pad_zeros(2) + ":" + str(Globals.sec_elapsed).pad_zeros(2).pad_decimals(2)
 	return
 	
 func display_game_over_text():
 	self.text = "Game Over!"
 	self.text += '\n' + "You Died!"
+	self.text += '\n' + "You Survived for " + str(Globals.min_elapsed).pad_zeros(2) + ":" + str(Globals.sec_elapsed).pad_zeros(2).pad_decimals(2)
+	self.text += '\n' + "Score:" + str(Globals.score)
 	self.text += '\n' + "Press ESC to Play Again!"
 	
 func display_game_win_text():
 	self.text = "Game Over!"
 	self.text += '\n' + "You Win!"
+	
+	self.text += '\n' + "You eliminated all the enemies in " + str(Globals.min_elapsed).pad_zeros(2) + ":" + str(Globals.sec_elapsed).pad_zeros(2).pad_decimals(2)
+	self.text += '\n' + "Score:" + str(Globals.score)
 	self.text += '\n' + "Press ESC to Play Again!"
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if Globals.player_health <= 0:
+		
 		display_game_over_text()
+		
 		if (Input.is_action_pressed("ui_cancel")):
 			Globals.kph = 0;
 
@@ -49,11 +52,22 @@ func _process(_delta):
 
 			Globals.player_pos = Vector3.ZERO;
 			
-			get_tree().reload_current_scene();
+			Globals.sec_elapsed = 0
+			Globals.min_elapsed = 0
+			
+			#get_tree().reload_current_scene();
+			get_tree().change_scene("res://Menu.tscn")
 			
 			
 	elif Globals.enemies_left < 1:
+		
+		if not score_calculated:
+			print_debug("score before end:", Globals.score)
+			Globals.score *= ((Globals.num_monsters + Globals.num_pantera) * (5 + Globals.ai_smartness) / ((Globals.min_elapsed * 60) + Globals.sec_elapsed)) + 1
+			score_calculated = true
+			
 		display_game_win_text()
+		
 		if (Input.is_action_pressed("ui_cancel")):
 			Globals.kph = 0;
 
@@ -69,7 +83,15 @@ func _process(_delta):
 #			Globals.enemy_health1 = 100;
 #			Globals.enemy_health2 = 100;
 
-			get_tree().reload_current_scene()
+			Globals.sec_elapsed = 0
+			Globals.min_elapsed = 0
+
+			#get_tree().reload_current_scene()
+			get_tree().change_scene("res://Menu.tscn")
 	else:
+		Globals.sec_elapsed += _delta;
+		if Globals.sec_elapsed >= 60:
+			Globals.sec_elapsed = 0
+			Globals.min_elapsed += 1
 		display_text()
 		
--- globals.gd
+++ globals.gd
@@ -15,6 +15,9 @@ var player_pos = Vector3.ZERO;
 var mine_ready = false;
 var shield_active = false;
 
+var sec_elapsed = 0;
+var min_elapsed = 0;
+
 var num_monsters = 4; # range from 1 to 10
 var num_pantera = 4; # range from 1 to 10
 var ai_smartness = 3; # range form 1 to 5
